a68dc547c3f16a42aa4babd6dc2072219e22fdc4
Richard Clayton <rclayton@bericotechnologies.com>
2013-03-21 23:59:17 -0400

    Fixed a problem with the init script

--- fallwizard/src/main/resources/archetype-resources/start-server.sh
+++ fallwizard/src/main/resources/archetype-resources/start-server.sh
@@ -1,3 +1,3 @@
 mvn clean
 mvn package
-java -jar target/${artifactId}-${version}.jar server configuration/server-basicAuth.yaml
+java -jar target/${artifactId}-${version}.jar server conf/server-basicAuth.yaml
